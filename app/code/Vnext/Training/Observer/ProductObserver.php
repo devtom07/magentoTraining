55bad2ad9d2277a925d1b4beb3ffae7c40d226b7
manhnd3 <manhnd3@vnext.com.vn>
2021-08-02 16:19:15 +0700

    custom product

--- app/code/Vnext/Training/Observer/ProductObserver.php
+++ app/code/Vnext/Training/Observer/ProductObserver.php
@@ -1,37 +1,41 @@
 <?php
 
-    namespace Vnext\training\Observer;
+namespace Vnext\training\Observer;
 
-    class ProductObserver
+use Magento\Catalog\Model\ProductFactory;
+
+class ProductObserver
+{
+    protected $orderFactory;
+    protected $productFactory;
+    protected $stockRegistry;
+
+    public function __construct(\Magento\Quote\Model\QuoteFactory $quoteFactory,
+                                \Magento\Sales\Model\Order $orderFactory,
+                                ProductFactory $productFactory,
+                                \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry)
     {
-        protected $orderFactory;
-        protected $productFactory;
-        protected $stockRegistry;
+        $this->orderFactory = $orderFactory;
+        $this->productFactory = $productFactory;
+        $this->stockRegistry = $stockRegistry;
+    }
 
-        public function __construct(\Magento\Quote\Model\QuoteFactory $quoteFactory,
-                                    \Magento\Sales\Model\Order $orderFactory,
-                                    ProductFactory $productFactory,
-                                    \Magento\CatalogInventory\Api\StockRegistryInterface $stockRegistry )
-        {
-            $this->orderFactory = $orderFactory;
-            $this->productFactory = $productFactory;
-            $this->stockRegistry = $stockRegistry;
-        }
-            $order = $observer->getEvent()->getOrder();
-            $orderId = $order->getIncrementId();
-            $orderItems = $order->getAllItems();
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        $order = $observer->getEvent()->getOrder();
+        $orderId = $order->getIncrementId();
+        $orderItems = $order->getAllItems();
 
-            foreach ($orderItems as $item){
+        foreach ($orderItems as $item) {
             $sku = $item->getProduct()->getSku();
             $qty = 0;
             $stockItem = $this->stockRegistry->getStockItemBySku($sku);
             $stockItem->setQty($qty);
             $this->stockRegistry->updateStockItemBySku($sku, $stockItem);
-    }
-
-
-
-
+        }
 
     }
+
+
+}
 
