f4198b8341cf6a885c54d945b45a35c771dd69b2
manhnd3 <manhnd3@vnext.com.vn>
2021-08-16 11:20:00 +0700

    Announce the number of points can be earned when paying

---
+++ app/code/Vnext/RewardPoints/Plugin/Checkout/CustomerData/Cart.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Vnext\RewardPoints\Plugin\Checkout\CustomerData;
+
+use Vnext\RewardPoints\Model\ResourceModel\Earningrate\CollectionFactory;
+
+class Cart
+{
+    protected $earning;
+
+    public function __construct(CollectionFactory $earning)
+    {
+        $this->earning = $earning;
+    }
+
+    public function afterGetSectionData(\Magento\Checkout\CustomerData\Cart $subject, array $result)
+    {
+        $point = $this->earning->create();
+        $total = $result['subtotalAmount'];
+        foreach ($point as $data){
+        $money_spent = $data->getMoneySpent();
+        }
+        $earning = round($total / $money_spent *10);
+        $result['extra_data'] = $earning;
+
+        return $result;
+    }
+}
